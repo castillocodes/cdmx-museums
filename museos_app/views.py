3c520fb22428b5ad24e4de6a4d805a808fab3b1c
castillocodes <janeirosergio@gmail.com>
2021-07-10 12:35:06 -0500

    Urls + Views Initial

--- museos_app/views.py
+++ museos_app/views.py
@@ -2,4 +2,35 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is my response!")
+    return render (request, "index.html")
+
+def signup(request):
+    print(request.POST)
+    errors = User.objects.basic_validator(request.POST)
+    print (errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    new_user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=request.POST['pw'])
+    request.session['user'] = new_user.first_name
+    request.session['id'] = new_user.id
+    return redirect('/success')
+
+def login(request):
+    print(request.POST)
+    logged_user = User.objects.filter(email=request.POST['email'])
+    if len(logged_user) > 0:
+        logged_user = logged_user[0]
+        if logged_user.password == request.POST['pw']:
+            request.session['user'] = logged_user.first_name
+            request.session['id'] = logged_user.id
+            return redirect('/success')
+    messages.error(request, 'Email / Password invalid')
+    return redirect('/')
+
+def logout(request):
+    print(request.session)
+    request.session.flush()
+    print(request.session)
+    return redirect('/')
